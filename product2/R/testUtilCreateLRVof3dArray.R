b49ec44d566021abf4934da5a1e1be9f64b3de13
Gograman <gograman93@gmail.com>
2017-10-24 15:00:39 +0300

    test createBetaLRV2d/3dArray

---
+++ product2/R/testUtilCreateLRVof3dArray.R
@@ -0,0 +1,29 @@
+testUtilCreateLRVof3dArray <- function(replicationCount, size,
+                                       psi,
+                                       sigma) {
+  cat("\nTesting \'tests_31_CreateLRVof3dArray\' \n")
+
+  Start <- Sys.time()
+  originalReplicated <-
+    createOriginalMA1Replicated (
+      replicationCount = replicationCount,
+      size = size,
+      psi = psi,
+      sigma = sigma
+    )
+  productColReplicated <-
+    createProductColReplicated(originalReplicated)
+  LRV3dArray = createLRVof3dArray(productColReplicated)
+  End <- Sys.time()
+  duration <- End - Start
+  cat("Duration =", duration, "\n")
+  cat("LRV3dArray =",LRV3dArray, "\n")
+  cat("Test parameters:", "\n")
+  cat("size =", size, "\n")
+  cat("psi =", psi, "\n")
+  cat("sigma =", sigma, "\n")
+  cat("replicationCount =", replicationCount, "\n")
+  cat("originalREplicated =", originalReplicated[1:5], "\n")
+  cat("End of test createBetaArray", "\n")
+  cat("=====================")
+}
